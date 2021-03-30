539d36d4f843b5eba76f35fb8e5db14cab10ad95
BNU-DylanHamilton <60743070+BNU-DylanHamilton@users.noreply.github.com>
2021-03-31 00:27:48 +0100

    Add files via upload

---
+++ Wk5index.php
@@ -0,0 +1,13 @@
+<?php include("Wk5class_lib.php"); ?>
+
+<?php
+
+	$stefan = new person();
+	$jimmy = new person;
+	$stefan->set_name("Stefan Mischook");
+	$jimmy->set_name("Nick Waddles");
+
+	echo "Stefan's full name: " . $stefan->get_name();
+	echo "Nick's full name: " . $jimmy->get_name();
+
+?>
